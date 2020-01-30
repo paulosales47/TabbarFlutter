74de2053c77a472d7a3e23820493df73a99ad922
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2020-01-29 22:57:16 -0300

    Commit incial - Utilizando tabbar

---
+++ lib/Home.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _tabController = TabController(
+      length: 3,
+      vsync: this
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Abas"),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: <Widget>[
+            Tab(
+              text: "Home",
+              icon: Icon(Icons.home),
+            ),
+            Tab(
+              text: "Email",
+              icon: Icon(Icons.email),
+            ),
+            Tab(
+              text: "Conta",
+              icon: Icon(Icons.account_circle),
+            ),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          Text("Primeira pagina"),
+          Text("Segunda pagina"),
+          Text("Terceira pagina"),
+        ],
+      ),
+    );
+  }
+}
